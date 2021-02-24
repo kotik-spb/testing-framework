08f613065b3cbca96d83062467d6112ff0280386
fedorovcev <fedorovcev@g-soft.ru>
2021-02-24 23:47:49 +0300

    переделал функции в асинхронные

--- simple/math.js
+++ simple/math.js
@@ -1,7 +1,7 @@
-const sum = (a,b) => a-b;
-const subtract = (a,b) => a-b;
+const sumAsync = (a,b) => a-b;
+const subtractAsync = (a,b) => a-b;
 
 module.exports = {
- sum,
- subtract 
+ sumAsync,
+ subtractAsync 
 }
--- simple/simple.js
+++ simple/simple.js
@@ -1,21 +1,21 @@
-const {sum, subtract} = require('./math');
+const {sumAsync, subtractAsync} = require('./math');
 
-test('sum', () => {
-  const result = sum(3,7);
+test('sumAsync', async () => {
+  const result = await sumAsync(3,7);
   const expected = 10;
   expect(result).toBe(expected)
 })
 
-test('subtract', () => {
-  const result = subtract(7,3);
+test('subtractAsync', async () => {
+  const result = await subtractAsync(7,3);
   const expected = 4;
   expect(result).toBe(expected)
 });
 
 
-function test(title, cb) {
+async function test(title, cb) {
   try {
-    cb();
+    await cb();
     console.log('\x1b[32m%s\x1b[0m','V ' + title + '\n');
   }
   catch (error) {
